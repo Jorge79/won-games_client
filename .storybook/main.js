8f74ac052afdc46f0743ac3a02fed015a31981c4
Jorge Francisco <Jorge7.9@hotmail.com>
2022-05-18 13:24:15 -0300

    fix: storybook absolute paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,8 @@ module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5'
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
   }
-}
-
-webpackFinal: (config) => {
-  config.resolve.modules.push(`${process.cwd()}/src`)
-  return config
 }
