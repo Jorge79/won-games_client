b4d59784d40e4730f80e32f23151fa5b2050eb21
Jorge Francisco <Jorge7.9@hotmail.com>
2022-05-25 12:23:13 -0300

    fix: lessThan variable name

--- src/components/MediaMatch/index.tsx
+++ src/components/MediaMatch/index.tsx
@@ -4,17 +4,17 @@ import media, { DefaultBreakpoints } from 'styled-media-query'
 type breakpoint = keyof DefaultBreakpoints
 
 export type MediaMatchProps = {
-  lesstThan?: breakpoint
+  lessThan?: breakpoint
   greaterThan?: breakpoint
 }
 
 const mediaMatchModifiers = {
   lessThan: (size: breakpoint) => css`
-    ${media.lessThan(size)`display: block`}
+    ${media.lessThan(size)` display: block `}
   `,
 
   greaterThan: (size: breakpoint) => css`
-    ${media.greaterThan(size)`display: block`}
+    ${media.greaterThan(size)` display: block `}
   `
 }
 
