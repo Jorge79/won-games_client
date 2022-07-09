ffe270c44100acb70e5e2d7fa075e89ad9b5ec75
Jorge Francisco <Jorge7.9@hotmail.com>
2022-07-08 22:33:49 -0300

    feat(Button): add icon option

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import Button from '.'
 
 export default {
@@ -14,5 +15,13 @@ export default {
 export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
-  children: 'Buy Now'
+  children: 'Buy now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
 }
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,11 +1,12 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
     // const { container } = render(<Button />)
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
@@ -13,7 +14,7 @@ describe('<Button />', () => {
       'font-size': '1.4rem',
       padding: '0.8rem 3.2rem'
     })
-    // expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size', () => {
@@ -41,5 +42,14 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/Buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
