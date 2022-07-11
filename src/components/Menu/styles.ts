a6d2163c83cab32017776e9278d781aac380ef06
Jorge Francisco <Jorge7.9@hotmail.com>
2022-07-10 23:43:06 -0300

    feat(Menu): create component and mobile version

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.menu`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.small} 0;
+  `}
+`
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+  `}
+`
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    height: 2.4rem;
+    width: 2.4rem;
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+
+    > div {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`
