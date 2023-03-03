f26d026b91132fd044cbe50670183eb9879635bc
Jorge Francisco <Jorge7.9@hotmail.com>
2023-03-03 20:10:08 -0300

    feat(Heading): adjuste to contain size and colors

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
+  lineColor?: LineColors
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
+  lineColor = 'primary',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineColor={lineColor}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
